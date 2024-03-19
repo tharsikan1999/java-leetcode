4e3ee652ed99bc6496fab060427e7c428a901c2a
tharsikan1999 <tharsikanking@gmail.com>
2024-03-19 15:26:05 +0530

    palindrome number

--- java leetcode/src/palindromenumber.java
+++ java leetcode/src/palindromenumber.java
@@ -1,6 +1,47 @@
- class palindromenumber {
-     public static void main  (String args[]){
+import javax.swing.text.Style;
 
-         System.out.println("ok it is work");
-     }
+class PalindromeNumber {
+    public static boolean isPalindrome(int num) {
+        int[] numerArray = getNumbersArray(num);
+        int[] reverseArray = reverseArray(numerArray);
+
+        for (int i = 0; i < numerArray.length; i++) {
+            if (numerArray[i] != reverseArray[i]) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    private static int[] reverseArray(int[] numerArray) {
+        int[] reverseArray = new int[numerArray.length];
+
+        for (int i = 0; i < numerArray.length; i++) {
+            reverseArray[i] = numerArray[numerArray.length - i - 1];
+        }
+        return reverseArray;
+    }
+
+    private static int[] getNumbersArray(int num) {
+        String numberString = String.valueOf(num);
+        int[] numerArray = new int[numberString.length()];
+
+        for (int i = 0; i < numberString.length(); i++) {
+            numerArray[i] = Character.getNumericValue(numberString.charAt(i));
+        }
+        return numerArray;
+    }
+
+    public static void main(String args[]) {
+        int number = 12321;
+        if (isPalindrome(number)) {
+            System.out.println("Input: x = " + number);
+            System.out.println("Output: true");
+            System.out.println("Explanation: " + number + " reads as " + number + " from left to right and from right to left.");
+        } else {
+            System.out.println("Input: x = " + number);
+            System.out.println("Output: false");
+        }
+    }
 }
